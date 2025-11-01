e131269e7b2b54d5429995837eae960744f31182
Justin Schaumberger <jes083@bucknell.edu>
2025-10-31 23:37:04 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -23,13 +23,13 @@ export const managers = [
       "name": "Ben Bachrach",
       "location": "Hoboken", // (optional)
       "bio": "Lorem ipsum...",
-      "photo": "/static/managers/Ben.JPG", // square ratio recommended (no larger than 500x500)
+      "photo": "/static/managers/everyone.png", // square ratio recommended (no larger than 500x500)
       "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Casey", // Can be anything (usually your rival's name)
         link: 10, // manager array number within this array, or null to link back to all managers page
-        image: "/static/managers/Casey.JPG", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+        image: "/static/managers/everyone.png", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
       "favoritePlayer": 818, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
